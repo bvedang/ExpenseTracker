4e457e841f89b3b9007bc0458d46a4bc30b8d2fd
bvedang <dvedang4u@gmail.com>
2022-12-03 07:05:01 -0600

    added jwt login to backend (InProgress)

--- backend/expense/__init__.py
+++ backend/expense/__init__.py
@@ -1,12 +1,25 @@
 import os
+from datetime import timedelta
+from flask_jwt_extended import JWTManager
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from flask_migrate import Migrate, migrate
+from flask_jwt_extended import get_jwt
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import set_access_cookies
+
+
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "mysecret"
+# Secret key to genere jwt token
+app.config["JWT_SECRET_KEY"] = "super-secret"
+app.config["JWT_COOKIE_SECURE"] = False
+app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
@@ -15,3 +28,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 api = Api(app)
+jwt = JWTManager(app)
+
+# @app.after_request
+# def refresh_expiring_jwts(response):
+#     try:
+#         exp_timestamp = get_jwt()["exp"]
+#         now = datetime.now(timezone.utc)
+#         target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+#         if target_timestamp > exp_timestamp:
+#             access_token = create_access_token(identity=get_jwt_identity())
+#             set_access_cookies(response, access_token)
+#         return response
+#     except (RuntimeError, KeyError):
+#         # Case where there is not a valid JWT. Just return the original response
+#         return response
+
+from expense.users.users import users
+app.register_blueprint(users,url_prefix="/users")
---
+++ backend/expense/users/users.py
@@ -0,0 +1,39 @@
+from expense import db
+from expense.models import Users
+from flask_jwt_extended import create_access_token,jwt_required,current_user
+from flask import Blueprint, request, jsonify
+
+users = Blueprint('users',__name__)
+
+@users.route('/create_user', methods=["GET"])
+def create_user():
+    userdata = request.get_json()
+    new_User = Users(email=userdata["email"], name=userdata["name"], password=userdata["password"])
+    db.session.add(new_User)
+    db.session.commit()
+    return jsonify({"message":"User created"}),200
+
+@users.route('/login', methods=["GET"])
+def login():
+    loginData = request.get_json()
+    email = loginData["email"]
+    password = loginData["password"]
+    user = Users.query.filter_by(email=email).first()
+    if user and user.check_password(password):
+        token = create_access_token(identity=user.public_id)
+        return jsonify({"token": token}),200
+    return jsonify({"msg":"Invalid reques"}),500
+
+@users.route('/get_users', methods=["GET"])
+@jwt_required()
+def getAllUsers():
+    usersInfo = Users.query.all()
+    resp =[]
+    for userinfo in usersInfo:
+        usersdict ={}
+        usersdict["name"]= userinfo.name
+        usersdict["public_id"]= userinfo.public_id
+        usersdict["name"]= userinfo.email
+        usersdict["name"]= userinfo.password_hash
+        resp.append(usersdict)
+    return jsonify({"data":resp})
